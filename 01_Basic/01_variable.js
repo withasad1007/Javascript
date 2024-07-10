dd0e91a10d3ea19fbb8f4292a0ac90d01c3b5871
Asad Khan <82949587+withasad1007@users.noreply.github.com>
2024-07-10 17:10:57 +0000

    done

--- 01_Basic/01_variable.js
+++ 01_Basic/01_variable.js
@@ -2,5 +2,5 @@ const accountId = 12421
 let accountEmail = "withasad1007@gmail.com"
 var accountPassword = "1230871"
 accountCity = "Saidu"
-accountId = 1
-console.log(accountId);
+//accountId = 1
+console.table([accountId,accountEmail,accountPassword,accountCity])
