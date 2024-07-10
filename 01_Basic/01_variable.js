7072d505db47c69056ca7d9e92268423bdd8576c
Asad Khan <82949587+withasad1007@users.noreply.github.com>
2024-07-10 17:19:30 +0000

    done

--- 01_Basic/01_variable.js
+++ 01_Basic/01_variable.js
@@ -3,4 +3,8 @@ let accountEmail = "withasad1007@gmail.com"
 var accountPassword = "1230871"
 accountCity = "Saidu"
 //accountId = 1
+/* 
+not to use var
+bcx of issue in block scope and functional scope
+*/
 console.table([accountId,accountEmail,accountPassword,accountCity])
